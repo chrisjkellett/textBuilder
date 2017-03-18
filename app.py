da4c8dc31fb578b5fad0410c015f354cc16a79f0
Geoffrey Kellett <Geoffrey Kellett>
2017-03-18 11:45:52 +0000

    pre-validate

--- app.py
+++ app.py
@@ -1,13 +1,47 @@
-from flask import Flask, render_template
-from forms import User_form
+from flask import (Flask,
+                   render_template,
+                   request,
+                   flash,
+                   redirect,
+                   url_for)
+from forms import Login_form, Register_form
+from models import User
+import hashlib
+import configparser
+
+config = configparser.ConfigParser()
+config.read('.env')
+
+
 app = Flask(__name__)
-app.secret_key = 'secret'
+app.secret_key = config['DEFAULT']['SECRET_KEY']
+app.debug = config['DEFAULT']['DEBUG']
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    form = User_form()
-    return render_template('items/index.html', form=form)
+    loginform = Login_form(request.form)
+    regform = Register_form(request.form)
+    if request.method == 'POST' and loginform.validate():
+        email = loginform.email.data
+        password = hashlib.md5(str(loginform.password.data).encode('utf-8')).hexdigest()
+        my_user = User.query.filter_by(email=email, password=password).first()
+        if my_user:
+            return redirect(url_for('loggedin'))
+        else:
+            flash('El usuario o contraseña no existe', 'danger')
+    else:
+        errores = loginform.errors.items()
+        for campo, mensajes in errores:
+            for mensaje in mensajes:
+                flash(mensaje, 'danger')
 
+    return render_template('items/index.html', form=loginform, regform=regform)
+
+
+@app.route('/loggedin')
+def loggedin():
+    return 'logged in!'
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
--- forms.py
+++ forms.py
@@ -1,17 +1,26 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Email, Length, NumberRange
+from wtforms.validators import DataRequired, Email, Length
 
 
-class User_form(FlaskForm):
+class Login_form(FlaskForm):
     email = StringField('email', validators=[DataRequired('Please add your username'),
         Email('Incorrect format'),
         Length(1, 80, 'Incorrect length')])
+    password = PasswordField('password', validators=[DataRequired('Please add your password'),
+                                                      Length(6, 25, 'Incorrect length')])
+    login = SubmitField('login')
+
+
+class Register_form(FlaskForm):
+    email = StringField('email', validators=[DataRequired('Please add your username'),
+                                             Email('Incorrect format'),
+                                             Length(1, 80, 'Incorrect length')])
     password1 = PasswordField('password', validators=[DataRequired('Please add your password'),
                                                       Length(6, 25, 'Incorrect length')])
     password2 = PasswordField('repeat password', validators=[DataRequired('Please add your password again')])
-    login = SubmitField('login')
     register = SubmitField('register')
+
 
 class getText(FlaskForm):
     user_text = TextAreaField('add text', validators=[DataRequired('Please add your text')])
