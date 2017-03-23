2f53d50f1eb8dca132d32014c56db0a5e71ca775
chrisjkellett <cjkellett@hotmail.co.uk>
2017-03-23 12:49:49 +0100

    ..confirm

--- makeDatabase.py
+++ makeDatabase.py
@@ -7,7 +7,8 @@ db.create_all()
 # create admin
 password = hashlib.md5(str('admin').encode('utf-8')).hexdigest()
 username = 'cjkellett'
-admin_user = User('cjkellett@hotmail.co.uk', password, username)
+admin_user = User('cjkellett@hotmail.com', password, username)
+admin_user.active = True
 
 db.session.add(admin_user)
 db.session.commit()
