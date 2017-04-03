80eec1e3d85f1c7c9ca1d6674b61c32784a9f3ca
chrisjkellett <cjkellett@hotmail.co.uk>
2017-04-03 21:00:51 +0200

    0304-5

--- static/js/logged-in.js
+++ static/js/logged-in.js
@@ -1,6 +1,20 @@
-//TOGGLE toolbox
+//globals available only when logged in
 var savedFiles = document.querySelector('#savedFiles');
 var saveFileEvent = document.querySelector('#saveFileEvent');
+var saveBtn = document.querySelector('#submitBtn4');
+var add_Title = document.querySelector('#addTitle');
+
+//title for save
+saveBtn.addEventListener('click', function(){
+    if(add_Title.style.display == 'flex'){
+        add_Title.style.display = 'none';
+    }else{
+        removeListeners();
+        add_Title.style.display = 'flex';
+    };
+});
+
+//listeners
 saveFileEvent.addEventListener('click', function(){
     if(savedFiles.style.display == 'flex'){
         savedFiles.style.display = 'none';
@@ -10,6 +24,9 @@ saveFileEvent.addEventListener('click', function(){
     };
 });
 
+
+
+//remove listeners
 var removeListeners = function(){
     aboutBox.style.display = 'none';
     savedFiles.style.display = 'none';
